a908ed36ea3d85ab56f907e6d70cbd4bbbcc3229
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-21 08:52:40 +0200

    Pyzzer: Launch a file (To be fuzzed) using command line arguments

--- pyzzer.py
+++ pyzzer.py
@@ -1,6 +1,14 @@
+#!/bin/python3
+
+import subprocess
+import os
+import sys
+
 # main()
 def main():
     print("Pyfuzzer - A simple Python Program Fuzzer")
+    print(f"Command line arguments: {str(sys.argv[1])}")
+    os.system(str(sys.argv[1]))
 
 # Call main on init
 if __name__ == "__main__":
