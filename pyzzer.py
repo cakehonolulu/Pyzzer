30ddf8f8dda110872194d59d4fce85dd0214de0b
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-10-21 10:41:10 +0200

    Pyzzer: Fuzz mono-input programs with cmdline args. Done using Popen and PIPE submodules

--- pyzzer.py
+++ pyzzer.py
@@ -1,14 +1,22 @@
 #!/bin/python3
 
-import subprocess
-import os
 import sys
+from subprocess import Popen, PIPE
 
 # main()
 def main():
     print("Pyfuzzer - A simple Python Program Fuzzer")
     print(f"Command line arguments: {str(sys.argv[1])}")
-    os.system(str(sys.argv[1]))
+
+    m_progname = (f"./{str(sys.argv[1])}")
+
+    m_fuzzstr = (f"{str(sys.argv[2])}")
+
+    print(f"Program name: {m_progname}")
+    print(f"Fuzzing string: {m_fuzzstr}")
+
+    m_prog = Popen(m_progname, stdin=PIPE)
+    m_prog.communicate(m_fuzzstr.encode())
 
 # Call main on init
 if __name__ == "__main__":
